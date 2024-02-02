224a9c8faa929282c1d187e81fc2d5a50d065fa5
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-02-03 08:41:47 +0900

    김경윤 : EngineBase 업데이트

--- Mario/EngineBase/EngineDebug.cpp
+++ Mario/EngineBase/EngineDebug.cpp
@@ -1,2 +1,12 @@
 ﻿#include "EngineDebug.h"
 
+namespace EngineDebug
+{
+	void OutPutDebugText(std::string_view _DebugText)
+	{
+		std::string Text = _DebugText.data();
+		Text += "\n";
+
+		OutputDebugStringA(Text.c_str());
+	}
+}
