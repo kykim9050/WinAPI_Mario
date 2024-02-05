752ffec07c568a11e104b28cc6f26dbd72655235
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-02-06 00:01:35 +0900

    김경윤 : TitleLevel Background에 256x240 이미지 출력하기

--- Mario/MarioContents/ContentsCore.cpp
+++ Mario/MarioContents/ContentsCore.cpp
@@ -1,6 +1,9 @@
 ﻿#include "ContentsCore.h"
 #include "TitleLevel.h"
 #include "PlayLevel.h"
+#include <EngineBase/EngineDirectory.h>
+#include <EngineBase/EngineFile.h>
+#include <EngineCore/EngineResourcesManager.h>
 
 ContentsCore::ContentsCore()
 	: UEngineCore()
@@ -14,12 +17,27 @@ ContentsCore::~ContentsCore()
 // 프로그램 시작
 void ContentsCore::BeginPlay()
 {
+	UEngineCore::BeginPlay();
+
 	// 윈도우 크기 배율
-	int WindowSizeMulValue = 3;
+	int WindowSizeMulValue = 1;
 
 	// 마리오용 윈도우 크기 설정
 	MainWindow.SetWindowScale({ 256 * WindowSizeMulValue, 240 * WindowSizeMulValue });
 	
+
+	UEngineDirectory ResourcesDir = UEngineDirectory();
+	ResourcesDir.MoveParent();
+	ResourcesDir.Move("Resources");
+
+	std::list<UEngineFile> ResourcesList = ResourcesDir.AllFile({ ".png", "bmp" }, true);
+
+	for (UEngineFile& CurFIle : ResourcesList)
+	{
+		UEngineResourcesManager::GetInst().LoadImg(CurFIle.GetFullPath());
+	}
+
+
 	CreateLevel<UTitleLevel>("TitleLevel");
 	//CreateLevel<UPlayLevel>("PlayLevel");
 
