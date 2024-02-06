5aae07e255940bcfbb033203d917d2e0941b2a87
kykim9050 <kykim9050@naver.com>
2024-02-06 16:47:58 +0900

    김경윤 : 1_1Stage BackGround 이미지 출력

--- Mario/MarioContents/BackGroundMap.cpp
+++ Mario/MarioContents/BackGroundMap.cpp
@@ -1,4 +1,6 @@
 ﻿#include "BackGroundMap.h"
+#include "EngineCore/EngineCore.h"
+
 
 BackGroundMap::BackGroundMap()
 {
@@ -12,7 +14,16 @@ void BackGroundMap::BeginPlay()
 {
 	AActor::BeginPlay();
 
+	UImageRenderer* Stage1_1BackGroundRenderer = CreateImageRenderer();
+
+	Stage1_1BackGroundRenderer->SetImage("1_1StageBackGroundMap.png");
+
+	// 이미지의 사이즈 받아올 수 있도록 클래스에 맴버 변수 선언, 그리고 상수 치환
+	Stage1_1BackGroundRenderer->SetTransform({ { 5064, 720 }, { 10128, 1440 } });
+
+
 }
+
 
 void BackGroundMap::Tick(float _DeltaTime)
 {
