4467a5e8fdd7d9b0e3afaeeb619daaff1e439203
kky <kykim9050@naver.com>
2024-02-06 15:02:44 +0900

    김경윤 : 타이틀맵의 동전 반짝이는 애니메이션 업데이트

--- Mario/MarioContents/TitleBackGround.cpp
+++ Mario/MarioContents/TitleBackGround.cpp
@@ -1,4 +1,5 @@
 #include "TitleBackGround.h"
+#include "ContentsCore.h"
 
 ATitleBackGround::ATitleBackGround()
 {
@@ -17,7 +18,7 @@ void ATitleBackGround::BeginPlay()
 	TitleBackGroundRenderer->SetImage("Mario_Title_Background.png");
 
 	// 윈도우 사이즈 받아올 수 있도록 수정 필요
-	TitleBackGroundRenderer->SetTransform({ { 128 * 3, 120 * 3 }, { 256 * 3,240 * 3 } });
+	TitleBackGroundRenderer->SetTransform({ { 128 * ContentsCore::WindowSizeMulValue, 120 * ContentsCore::WindowSizeMulValue }, { 256 * ContentsCore::WindowSizeMulValue,240 * ContentsCore::WindowSizeMulValue } });
 }
 
 void ATitleBackGround::Tick(float _DeltaTime)
--- Mario/MarioContents/TitleCoin.cpp
+++ Mario/MarioContents/TitleCoin.cpp
@@ -1,4 +1,5 @@
 ﻿#include "TitleCoin.h"
+#include "ContentsCore.h"
 
 ATitleCoin::ATitleCoin()
 {
@@ -11,6 +12,19 @@ ATitleCoin::~ATitleCoin()
 void ATitleCoin::BeginPlay()
 {
 	AActor::BeginPlay();
+
+	// Title전용 RendererOrder Enum class만들어야 함
+	UImageRenderer* TitleCoinRenderer = CreateImageRenderer(1);
+	TitleCoinRenderer->SetImage("TitleCoin.png");
+	// 윈도우 사이즈 받아올 수 있도록 수정 필요
+
+	TitleCoinRenderer->SetTransform({ {88 * ContentsCore::WindowSizeMulValue, 24 * ContentsCore::WindowSizeMulValue},{64*ContentsCore::WindowSizeMulValue, 64*ContentsCore::WindowSizeMulValue} });
+
+	// 랜더링이 idx0으로 다시 가면 안되고 0 1 2 1 0 1 2 ... 이런식으로 되어야 함
+	TitleCoinRenderer->CreateAnimation("Coin_Twinkle", "TitleCoin.png", 0, 4, 0.15f, true);
+	TitleCoinRenderer->ChangeAnimation("Coin_Twinkle");
+
+
 }
 
 void ATitleCoin::Tick(float _DeltaTime)
