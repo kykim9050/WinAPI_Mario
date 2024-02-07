921ac162bf3587ec65f9619675bf1d34d8d4cf5c
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-02-07 20:36:57 +0900

    김경윤 : 함수 설명 주석 추가

--- Mario/EngineBase/EngineDebug.h
+++ Mario/EngineBase/EngineDebug.h
@@ -11,6 +11,11 @@ namespace EngineDebug
 	std::string ErrorText = std::string(Value); \
 	MessageBoxA(nullptr, ErrorText.c_str(), "치명적 에러", MB_OK); assert(false);
 
+	/// <summary>
+	/// 출력창에서 _DebugText내용을 확인할 수 있게 해주는 함수
+	/// 디버깅용
+	/// </summary>
+	/// <param name="_DebugText"></param>
 	void OutPutDebugText(std::string_view _DebugText);
 }
 
