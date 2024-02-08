db99046c67c6e1ff0a03c671f9ec3b4be3964094
kykim9050 <kykim9050@naver.com>
2024-02-08 12:52:34 +0900

    김경윤 : EndingLevel, EndingBackGround 클래스 추가

---
+++ Mario/MarioContents/EndingLevel.cpp
@@ -0,0 +1,21 @@
+﻿#include "EndingLevel.h"
+#include "EndingBackGround.h"
+
+UEndingLevel::UEndingLevel()
+{
+}
+
+UEndingLevel::~UEndingLevel()
+{
+}
+
+void UEndingLevel::BeginPlay()
+{
+	ULevel::BeginPlay();
+	AEndingBackGround* BackGround = SpawnActor<AEndingBackGround>();
+}
+
+void UEndingLevel::Tick(float _DeltaTime)
+{
+	ULevel::Tick(_DeltaTime);
+}
