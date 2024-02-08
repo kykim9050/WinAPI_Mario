074080626ad3dc2001de17dc6b7bb1cb33aa1b5f
kykim9050 <kykim9050@naver.com>
2024-02-08 16:50:54 +0900

    김경윤 : EndingLevel 메세지 출력 사이즈 지정 완료

--- Mario/MarioContents/EndingMessage.cpp
+++ Mario/MarioContents/EndingMessage.cpp
@@ -15,12 +15,13 @@ void AEndingMessage::BeginPlay()
 
 	UImageRenderer* MessageRenderer = CreateImageRenderer(static_cast<int>(EndingRenderOrder::Message));
 	
+	// UImageRenderer를 5개 만들고 따로따로 이미지 잘라서 저장 출력하게 만들기 (Message에서 자료구조로 각 이미지를 갖고 있도록 하자)
+	// 메세지별로 매칭되는 프레임 자료구조도 만들어서 함께 호출할 수 있도록 하기
 	MessageRenderer->SetImage("Mario_Ending_BackGround_Message.png");
 	FVector ImageScale = MessageRenderer->GetImage()->GetScale();
-	MessageRenderer->SetTransform({ {ImageScale.ihX(), ImageScale.ihY()},{ImageScale} });
+	MessageRenderer->SetTransform({ {ImageScale.ihX(), ImageScale.ihY()},{ImageScale.iX(),ImageScale.iY() / MessageNum}});
 
-	MessageRenderer->CreateAnimation("Ending_Message", "Mario_Ending_BackGround_Message.png", 0, 4, 1.0f, false);
-	MessageRenderer->ChangeAnimation("Ending_Message");
+
 }
 
 void AEndingMessage::Tick(float _DeltaTime)
