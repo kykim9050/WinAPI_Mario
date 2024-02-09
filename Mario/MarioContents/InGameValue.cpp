f8c4f175dceb42acae37743773d1452f7dc259c8
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-02-09 23:24:15 +0900

    김경윤 : InGameValue 클래스 추가

---
+++ Mario/MarioContents/InGameValue.cpp
@@ -0,0 +1,14 @@
+#include "InGameValue.h"
+
+const int UInGameValue::WindowSizeMulValue = 3;
+const int UInGameValue::MainWindowXScale = 256;
+const int UInGameValue::MainWindowYScale = 240;
+
+const int UInGameValue::MarioRightImageXValue = 5;
+const int UInGameValue::MarioRightImageYValue = 8;
+
+const int UInGameValue::MarioLeftImageXValue = 5;
+const int UInGameValue::MarioLeftImageYValue = 8;
+
+const int UInGameValue::TitleCoinImgX = 6;
+const int UInGameValue::TitleCoinImgY = 1;
