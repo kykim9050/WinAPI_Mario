9d378417000dbe6c97f56eef794fa3777ae8e9d7
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-02-10 18:47:49 +0900

    김경윤 : Enum Class PlayerState추가 및 Player 클래스에 StateUpdate 함수 추가 (FSM원칙 고수)

--- Mario/MarioContents/EnumClass.h
+++ Mario/MarioContents/EnumClass.h
@@ -1,11 +1,17 @@
 #pragma once
 
+/// <summary>
+/// 타이틀 랜더링 순서
+/// </summary>
 enum class TitleRenderOrder
 {
 	BackGroundMap,
 	Coin,
 };
 
+/// <summary>
+/// 엔딩 랜더링 순서
+/// </summary>
 enum class EndingRenderOrder
 {
 	BackGroundMap,
@@ -14,8 +20,25 @@ enum class EndingRenderOrder
 	MessageHide,
 };
 
+/// <summary>
+/// 스테이지 랜더러 순서
+/// </summary>
 enum class StageRenderOrder
 {
 	BackGroundMap,
 	Mario,
+	Monster,
+};
+
+/// <summary>
+/// 플레이어의 상태
+/// </summary>
+enum class PlayerState
+{
+	None,
+	FreeMove,
+	CameraMove,
+	Idle,
+	Move,
+	Jump,
 };
