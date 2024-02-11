cedf498fdf789098f442cb8e4c41a91099b19438
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-02-11 17:33:32 +0900

    김경윤 : PlayerMario 주석 추가

--- Mario/MarioContents/PlayerMario.h
+++ Mario/MarioContents/PlayerMario.h
@@ -22,24 +22,77 @@ protected:
 	void Tick(float _DeltaTime) override;
 
 private:
+	/// <summary>
+	/// 방향을 확인한 후에 해당 방향을 기준으로 Idle 애니메이션이름을 바꾸고 ChangeAnimation의 인자로 넣는다.
+	/// </summary>
 	void IdleStart();
+
+	/// <summary>
+	/// 방향을 확인한 후에 해당 방향을 기준으로 Move 애니메이션이름을 바꾸고 ChangeAnimation의 인자로 넣는다.
+	/// </summary>
 	void MoveStart();
+
+	/// <summary>
+	/// Actor의 상태가 변경될 때 실행되는 함수
+	/// 각 상태가 시작되기 전에 실행되어야 할 함수를 호출해준다.
+	/// </summary>
+	/// <param name="_PlayerState"></param>
 	void StateChange(EPlayerState _PlayerState);
+	
+	/// <summary>
+	/// Actor의 방향(좌,우)을 확인하고 맴버 변수값으로 지정하는 함수
+	/// </summary>
 	void DirCheck();
+
+	/// <summary>
+	/// 픽셀 충돌을 활용한 중력체크 함수
+	/// </summary>
 	void GravityCheck(float _DeltaTime);
+
+	/// <summary>
+	/// 방향 정보가 적용된 애니메이션 이름으로 수정하는 함수
+	/// </summary>
 	std::string ChangeAnimationName(std::string _MainName);
+
+	/// <summary>
+	/// Actor의 현재 좌표 X, Y에서 현재 방향을 기준으로 OffSet값을 적용한 위치를 반환하는 함수
+	/// </summary>
 	FVector GetActorOffSetPos();
 
 	//void FreeMove(float _DeltaTime);
+	
+	/// <summary>
+	/// 상태를 체크하며 각 상태에 맞는 함수를 호출
+	/// </summary>
 	void StateUpdate(float _DeltaTime);
+	
+	/// <summary>
+	/// Actor가 Idle 상태에서 취할 수 있는 기능
+	/// 1. 중력 적용
+	/// 2. 다른 State로의 전환
+	/// </summary>
 	void Idle(float _DeltaTime);
+
+	/// <summary>
+	/// Actor가 Move 상태에서 취할 수 있는 기능
+	/// 1. 중력 적용
+	/// 2. 다른 State로의 전환
+	/// 3. 좌우 이동
+	/// 4. 장애물에 위치 확인 후 이동 제한
+	/// </summary>
 	void Move(float _DeltaTime);
 
+	/// <summary>
+	/// Actor의 방향을 수정
+	/// </summary>
 	void SetMarioDir(const EPlayerDir _Dir)
 	{
 		MarioDir = _Dir;
 	}
 	
+	/// <summary>
+	/// Actor의 현 상태를 수정
+	/// </summary>
 	void SetMarioState(const EPlayerState _State)
 	{
 		MarioState = _State;
