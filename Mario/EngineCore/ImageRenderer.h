08ef038765c830c918d67eaa3a1d4438a8228933
kykim9050 <kykim9050@naver.com>
2024-02-13 21:50:58 +0900

    김경윤 : 함수 주석 추가

--- Mario/EngineCore/ImageRenderer.h
+++ Mario/EngineCore/ImageRenderer.h
@@ -54,6 +54,16 @@ public:
 		ImageCuttingTransform = _Value;
 	}
 
+	/// <summary>
+	/// 이미지를 조합하여 애니메이션을 만드는 함수.
+	/// 여러개의 이미지가 모여있는 리소스 이미지에서 특정 시점과 종점의 idx를 지정하여 순서대로 진행되는 애니메이션을 만들 수 있다.
+	/// </summary>
+	/// <param name="_AnimationName"></param>
+	/// <param name="_ImageName"></param>
+	/// <param name="_Start"></param>
+	/// <param name="_End"></param>
+	/// <param name="_Inter"></param>
+	/// <param name="_Loop"></param>
 	void CreateAnimation(
 		std::string_view _AnimationName,
 		std::string_view _ImageName,
@@ -63,6 +73,15 @@ public:
 		bool _Loop = true
 	);
 
+	/// <summary>
+	/// 이미지를 조합하여 애니메이션을 만드는 함수.
+	/// 여러개의 이미지가 모여있는 리소스 이미지의 Index(순서)를 직접 설정하여 원하는 순서의 애니메이션을 지정할 수 있다.
+	/// </summary>
+	/// <param name="_AnimationName"></param>
+	/// <param name="_ImageName"></param>
+	/// <param name="_Indexs"></param>
+	/// <param name="_Inter"></param>
+	/// <param name="_Loop"></param>
 	void CreateAnimation(
 		std::string_view _AnimationName,
 		std::string_view _ImageName,
@@ -71,6 +90,16 @@ public:
 		bool _Loop = true
 	);
 
+	/// <summary>
+	/// 애니메이션을 변경하는 함수.
+	/// _IsForce는 같은 종류의 애니메이션도 강제로 idx0부터 실행시킬지를 정할 수 있는 인자
+	/// 애니메이션 변경 시, 대칭되는 애니메이션이 있다면 이미지도 대칭일테니 _StartIndex, _Time인자에 대응되는 값을 대입할 것
+	/// 대칭되는 애니메이션이 아니라면 디폴트인자로 넘길 것
+	/// </summary>
+	/// <param name="_AnimationName"></param>
+	/// <param name="_IsForce"></param>
+	/// <param name="_StartIndex"></param>
+	/// <param name="_Time"></param>
 	void ChangeAnimation(std::string_view _AnimationName, bool _IsForce = false, int _StartIndex = 0, float _Time = -1.0f);
 	void AnimationReset();
 
@@ -107,22 +136,40 @@ public:
 		CameraEffect = false;
 	}
 
+	/// <summary>
+	/// 지정해놓은 애니메이션의 한 사이클이 끝났을 때, true를 반환한다.
+	/// </summary>
+	/// <returns></returns>
 	bool IsCurAnimationEnd() const
 	{
 		return CurAnimation->IsEnd;
 	}
 
+	/// <summary>
+	/// UAnimationInfo의 맴버 vector 자료구조 Indexs의 idx 값을 가져오는 함수
+	/// Indexs의 idx에 매칭되는 실제 값은 리소스 이미지(PNG등)의 실제 이미지 프레임 번호이다.
+	/// </summary>
+	/// <returns></returns>
 	int GetCurAnimationFrame() const
 	{
 		return CurAnimation->CurFrame;
 	}
 
+	/// <summary>
+	/// 현재의 애니메이션에서 indexs의 값인 이미지 프레임 번호를 가져오는 함수
+	/// </summary>
+	/// <returns></returns>
 	int GetCurAnimationImageFrame() const
 	{
 		const std::vector<int>& Indexs = CurAnimation->Indexs;
 		return Indexs[CurAnimation->CurFrame];
 	}
 
+	/// <summary>
+	/// 애니메이션 실행 중 _DeltaTime을 뺀 현재 시간
+	/// DeltaTime을 계속 빼다보면 0보다 작아질 때 해당되는 Frame에 대한 시간 값을 가져온다.
+	/// </summary>
+	/// <returns></returns>
 	float GetCurAnimationTime() const
 	{
 		return CurAnimation->CurTime;
