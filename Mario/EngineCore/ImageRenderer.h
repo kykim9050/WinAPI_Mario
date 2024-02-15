54beb505b29a8f540e40dbf61009795b8d28c709
kykim9050 <kykim9050@naver.com>
2024-02-15 14:40:55 +0900

    김경윤 : EngineCore, EnginePlatform 업데이트

--- Mario/EngineCore/ImageRenderer.h
+++ Mario/EngineCore/ImageRenderer.h
@@ -24,6 +24,8 @@ class UWindowImage;
 class UImageRenderer : public USceneComponent
 {
 public:
+
+public:
 	// constrcuter destructer
 	UImageRenderer();
 	~UImageRenderer();
@@ -175,6 +177,28 @@ public:
 		return CurAnimation->CurTime;
 	}
 
+	void TextRender(float _DeltaTime);
+	void ImageRender(float _DeltaTime);
+
+	void SetText(std::string_view _Text)
+	{
+		Text = _Text;
+	}
+	void SetFont(std::string_view _Font)
+	{
+		Font = _Font;
+	}
+	void SetTextSize(float _Value)
+	{
+		Size = _Value;
+	}
+	void SetTextColor(Color8Bit _Color)
+	{
+		TextColor = _Color;
+	}
+
+	FTransform GetRenderTransForm();
+
 protected:
 	void BeginPlay() override;
 
@@ -187,5 +211,10 @@ private:
 
 	std::map<std::string, UAnimationInfo> AnimationInfos;
 	UAnimationInfo* CurAnimation = nullptr;
+
+	std::string Text = "";
+	std::string Font = "±Ã¼­";
+	float Size = 10.0f;
+	Color8Bit TextColor = Color8Bit::BlackA;
 };
 
