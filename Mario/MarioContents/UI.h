ea52ebe73b144d4418167ec72296d1e5edd3fa43
kky <kykim9050@naver.com>
2024-02-19 14:21:10 +0900

    김경윤 : Play Stage 상단 정보창 출력을 위한 UI 클래스 생성

---
+++ Mario/MarioContents/UI.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <EngineCore/Actor.h>
+
+// Ό³Έν :
+class AUI : public AActor
+{
+public:
+	// constrcuter destructer
+	AUI();
+	~AUI();
+
+	// delete Function
+	AUI(const AUI& _Other) = delete;
+	AUI(AUI&& _Other) noexcept = delete;
+	AUI& operator=(const AUI& _Other) = delete;
+	AUI& operator=(AUI&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+
+};
+
