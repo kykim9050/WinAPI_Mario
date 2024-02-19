9d5fe92814e35a0c33c838f32463888c094db22b
kykim9050 <kykim9050@naver.com>
2024-02-19 14:21:10 +0900

    김경윤 : Play Stage 상단 정보창 출력을 위한 UI 클래스 생성

---
+++ Mario/MarioContents/UI.cpp
@@ -0,0 +1,19 @@
+﻿#include "UI.h"
+
+AUI::AUI()
+{
+}
+
+AUI::~AUI()
+{
+}
+
+void AUI::BeginPlay()
+{
+	AActor::BeginPlay();
+}
+
+void AUI::Tick(float _DeltaTime)
+{
+	AActor::Tick(_DeltaTime);
+}
