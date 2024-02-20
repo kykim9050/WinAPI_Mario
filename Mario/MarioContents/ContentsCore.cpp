c0c79cf96915dd33b62317bcf55e69660ead36ac
kykim9050 <kykim9050@naver.com>
2024-02-20 11:45:06 +0900

    김경윤 : DebugModeUpdate 함수 추가 (collision 형태 확인 가능)

--- Mario/MarioContents/ContentsCore.cpp
+++ Mario/MarioContents/ContentsCore.cpp
@@ -56,9 +56,19 @@ void UContentsCore::Tick(float _DeltaTime)
 {
 	UEngineCore::Tick(_DeltaTime);
 
+	DebugModeUpdate(_DeltaTime);
+
 }
 
 void UContentsCore::End()
 {
 	UEngineCore::End();
+}
+
+void UContentsCore::DebugModeUpdate(float _DeltaTime)
+{
+	if (true == UEngineInput::IsDown(VK_F2))
+	{
+		GEngine->EngineDebugSwitch();
+	}
 }
--- Mario/MarioContents/ContentsCore.h
+++ Mario/MarioContents/ContentsCore.h
@@ -22,6 +22,6 @@ protected:
 	void End() override;
 
 private:
-
+	void DebugModeUpdate(float _DeltaTime);
 };
 
