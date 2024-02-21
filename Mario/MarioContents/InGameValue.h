2149b139177c109a1f59ee76eebf5f933838fd2f
kky <kykim9050@naver.com>
2024-02-21 15:19:18 +0900

    김경윤 : Monster의 머리를 Player가 밟았을 때와 해당 상황을 제외한 충돌을 구분하는 기능 구현

--- Mario/MarioContents/InGameValue.h
+++ Mario/MarioContents/InGameValue.h
@@ -81,12 +81,6 @@ public:
 	static const int GoombaBodyCollisionScaleX;
 	static const int GoombaBodyCollisionScaleY;
 
-	/// <summary>
-	/// Goomba ÀÇ Head Collosion scale °ª
-	/// </summary>
-	static const int GoombaHeadCollisionScaleX;
-	static const int GoombaHeadCollisionScaleY;
-
 protected:
 
 private:
