31b96a2e6e403c30ca376c47a28cd2ea35764c54
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-02-24 07:28:34 +0900

    김경윤 : KoopaTroopa GetSecondHit 상태로 이동시 충돌체 만나면 튕기는 기능 구현

--- Mario/MarioContents/MonsterUnit.cpp
+++ Mario/MarioContents/MonsterUnit.cpp
@@ -17,3 +17,44 @@ void UMonsterUnit::Tick(float _DeltaTime)
 {
 	UStateUnit::Tick(_DeltaTime);
 }
+
+void UMonsterUnit::CalHorizonVelocityVector(float _DeltaTime)
+{
+	FVector CheckPos = GetActorLocation();
+	CheckPos.Y -= UInGameValue::ColOffSetY;
+
+	switch (ActorDir)
+	{
+	case EActorDir::Right:
+		CheckPos.X += UInGameValue::ColOffSetX;
+		break;
+	case EActorDir::Left:
+		CheckPos.X -= UInGameValue::ColOffSetX;
+		break;
+	default:
+		break;
+	}
+
+	Color8Bit Color = UContentsFunction::GetCollisionMapImg()->GetColor(CheckPos.iX(), CheckPos.iY(), UInGameValue::CollisionColor);
+
+	if (Color == UInGameValue::CollisionColor)
+	{
+		switch (ActorDir)
+		{
+		case EActorDir::Right:
+		{
+			ActorDir = EActorDir::Left;
+			ActorMoveDir = FVector::Left;
+			break;
+		}
+		case EActorDir::Left:
+		{
+			ActorDir = EActorDir::Right;
+			ActorMoveDir = FVector::Right;
+			break;
+		}
+		default:
+			break;
+		}
+	}
+}
