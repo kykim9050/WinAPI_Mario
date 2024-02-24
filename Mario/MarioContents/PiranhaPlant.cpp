c964a056e207981ce2934fa5deeaffe9b0164f68
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-02-24 10:36:57 +0900

    김경윤 : PiranhaPlant 클래스 추가 (배관에서 나오는 식인꽃 - Monster)

---
+++ Mario/MarioContents/PiranhaPlant.cpp
@@ -0,0 +1,19 @@
+#include "PiranhaPlant.h"
+
+APiranhaPlant::APiranhaPlant()
+{
+}
+
+APiranhaPlant::~APiranhaPlant()
+{
+}
+
+void APiranhaPlant::BeginPlay()
+{
+	UMonsterUnit::BeginPlay();
+}
+
+void APiranhaPlant::Tick(float _DeltaTime)
+{
+	UMonsterUnit::Tick(_DeltaTime);
+}
