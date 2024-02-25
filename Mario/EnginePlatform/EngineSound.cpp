def527daafa374cfa8bf34165f6c98d334178a1f
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-02-25 18:43:11 +0900

    김경윤 : EnginePlatform 업데이트

--- Mario/EnginePlatform/EngineSound.cpp
+++ Mario/EnginePlatform/EngineSound.cpp
@@ -1,4 +1,6 @@
 #include "EngineSound.h"
+#include <EngineBase\EngineString.h>
+#include <EngineBase\EngineDebug.h>
 
 // Debug 일때는
 // #pragma comment(lib, "ThirdParty\\FMOD\\inc\\fmodL_vc.lib")
@@ -6,6 +8,18 @@
 
 // Release 일때는 
 // #pragma comment(lib, "fmod_vc.lib")
+
+std::map<std::string, UEngineSound*> UEngineSound::Resources;
+
+void UEngineSound::ResourcesRelease()
+{
+	for (std::pair<const std::string, UEngineSound*>& Pair : Resources)
+	{
+		delete Pair.second;
+	}
+
+	Resources.clear();
+}
 
 UEngineSound::UEngineSound()
 {
@@ -18,5 +32,14 @@ UEngineSound::~UEngineSound()
 
 void UEngineSound::Load(std::string_view _Path, std::string_view _Name)
 {
-	
+	std::string UpperName = UEngineString::ToUpper(_Name);
+
+	if (true == Resources.contains(UpperName))
+	{
+		MsgBoxAssert("이미 로드한 리소스를 또 로드 하려고 했습니다.");
+		return;
+	}
+
+	UEngineSound* NewSound = new UEngineSound();
+	Resources[_Name.data()] = NewSound;
 }
--- Mario/EnginePlatform/EngineSound.h
+++ Mario/EnginePlatform/EngineSound.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <string>
 #include <string_view>
+#include <EngineBase\EnginePath.h>
+#include <map>
 
 #include "ThirdParty\FMOD\inc\fmod.hpp"
 
 // Ό³Έν :
 class UEngineSound
 {
+private:
+	static std::map<std::string, UEngineSound*> Resources;
+
 public:
 	// constrcuter destructer
 	UEngineSound();
@@ -18,7 +23,16 @@ public:
 	UEngineSound& operator=(const UEngineSound& _Other) = delete;
 	UEngineSound& operator=(UEngineSound&& _Other) noexcept = delete;
 
+	static void Load(std::string_view _Path)
+	{
+		UEnginePath NewPath = UEnginePath(std::filesystem::path(_Path));
+		std::string FileName = NewPath.GetFileName();
+		return Load(_Path, FileName);
+	}
+
 	static void Load(std::string_view _Path, std::string_view _Name);
+
+	static void ResourcesRelease();
 
 protected:
 
