eab3883baea4cae9ebf1cca08a10b13bfb1c396e
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-02-25 17:48:45 +0900

    김경윤 : Block관련 클래스 2종 추가 (CoinBlock, BrickBlock)

---
+++ Mario/MarioContents/CoinBlock.cpp
@@ -0,0 +1,19 @@
+#include "CoinBlock.h"
+
+ACoinBlock::ACoinBlock()
+{
+}
+
+ACoinBlock::~ACoinBlock()
+{
+}
+
+void ACoinBlock::BeginPlay()
+{
+	ABlockUnit::BeginPlay();
+}
+
+void ACoinBlock::Tick(float _DeltaTime)
+{
+	ABlockUnit::Tick(_DeltaTime);
+}
---
+++ Mario/MarioContents/CoinBlock.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "BlockUnit.h"
+
+// Ό³Έν :
+class ACoinBlock : public ABlockUnit
+{
+public:
+	// constrcuter destructer
+	ACoinBlock();
+	~ACoinBlock();
+
+	// delete Function
+	ACoinBlock(const ACoinBlock& _Other) = delete;
+	ACoinBlock(ACoinBlock&& _Other) noexcept = delete;
+	ACoinBlock& operator=(const ACoinBlock& _Other) = delete;
+	ACoinBlock& operator=(ACoinBlock&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+
+};
+
