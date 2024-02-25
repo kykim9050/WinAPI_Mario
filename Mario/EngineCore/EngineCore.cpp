ff4f72298411fffeb5a6026f5c6f259f3f9dddfe
kykim9050 <kykim9050@naver.com>
2024-02-25 19:15:19 +0900

    김경윤 : EngineCore 업데이트

--- Mario/EngineCore/EngineCore.cpp
+++ Mario/EngineCore/EngineCore.cpp
@@ -52,6 +52,8 @@ void UEngineCore::CoreTick()
 		NextLevel->LevelStart(CurLevel);
 		CurLevel = NextLevel;
 		NextLevel = nullptr;
+		MainTimer.TimeCheckStart();
+		DeltaTime = MainTimer.TimeCheck();
 	}
 
 	if (nullptr == CurLevel)
