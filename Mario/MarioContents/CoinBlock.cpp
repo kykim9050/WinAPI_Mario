07a86f3b12b4dfff89973312b808487589339c93
kky <kykim9050@naver.com>
2024-02-26 15:24:12 +0900

    김경윤 : CoinBlock 랜더링 구현

--- Mario/MarioContents/CoinBlock.cpp
+++ Mario/MarioContents/CoinBlock.cpp
@@ -11,6 +11,18 @@ ACoinBlock::~ACoinBlock()
 void ACoinBlock::BeginPlay()
 {
 	ABlockUnit::BeginPlay();
+
+	Renderer = CreateImageRenderer(static_cast<int>(EStageRenderOrder::Block));
+	Renderer->SetImage("CoinBlock.png");
+
+	FVector CoinBlockScale = Renderer->GetImage()->GetScale();
+
+	Renderer->SetTransform({ {0,0}, {CoinBlockScale.iX() / UInGameValue::CoinBlockImageXValue * UInGameValue::WindowSizeMulValue, CoinBlockScale.iY() / UInGameValue::CoinBlockImageYValue * UInGameValue::WindowSizeMulValue} });
+	Renderer->CreateAnimation("CoinBlock_Init", "CoinBlock.png", { 0, 0, 0, 0, 1, 2, 1 }, 0.15f, true);
+	Renderer->CreateAnimation("CoinBlock_Hit", "CoinBlock.png", 3, 3, 0.1f, true);
+	Renderer->CreateAnimation("CoinBlock_AfterHit", "CoinBlock.png", 4, 4, 0.1f, true);
+
+	Renderer->ChangeAnimation("CoinBlock_Init");
 }
 
 void ACoinBlock::Tick(float _DeltaTime)
