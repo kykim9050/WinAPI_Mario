e920e88265818d62736ff9e328ff9f48cfc575f6
kykim9050 <kykim9050@naver.com>
2024-02-29 13:39:32 +0900

    김경윤 : BlockCoin 클래스 별도 기능 구현 (블럭충돌시 나타나는 coin 아이템에 대한 클래스)

--- Mario/MarioContents/ItemUnit.cpp
+++ Mario/MarioContents/ItemUnit.cpp
@@ -11,18 +11,6 @@ AItemUnit::~AItemUnit()
 void AItemUnit::BeginPlay()
 {
 	AActor::BeginPlay();
-
-
-	Renderer = CreateImageRenderer(static_cast<int>(EStageRenderOrder::Item));
-	Renderer->SetImage("Coin.png");
-
-	FVector ItemScale = Renderer->GetImage()->GetScale();
-
-	Renderer->SetTransform({ {0,0}, {ItemScale.iX() / UInGameValue::CoinImageXValue, ItemScale.iY() / UInGameValue::CoinImageYValue} });
-	Renderer->CreateAnimation("Rotating_Coin", "Coin.png", 0, 3, 0.05f, true);
-	Renderer->ChangeAnimation("Rotating_Coin");
-
-	StateChange(EActorState::Idle);
 }
 
 void AItemUnit::Tick(float _DeltaTime)
@@ -71,47 +59,4 @@ void AItemUnit::StateChange(EActorState _ActorState)
 	}
 
 	SetActorState(_ActorState);
-}
-
-
-void AItemUnit::Idle(float _DeltaTime)
-{
-
-}
-
-void AItemUnit::Move(float _DeltaTime)
-{
-	// 동전의 경우
-	CoinRenderingTime -= _DeltaTime;
-
-	if (false == RenderingEnd && 0.0f >= CoinRenderingTime)
-	{
-		RenderingEnd = true;
-		GravityVelocityVector = FVector::Zero;
-		JumpVelocityVector = FVector::Zero;
-		StateChange(EActorState::Release);
-	}
-
-	GravityVelocityVector += GravityAccVector * _DeltaTime;
-	CalTotalVelocityVector(_DeltaTime);
-	AddActorLocation(TotalVelocityVector * _DeltaTime);
-
-}
-
-void AItemUnit::IdleStart()
-{
-	Renderer->ActiveOff();
-}
-
-void AItemUnit::MoveStart()
-{
-	JumpVelocityVector = FVector::Up * 600.0f;
-	Renderer->ActiveOn();
-}
-
-void AItemUnit::ReleaseStart()
-{
-	// 랜더링만 Destroy
-	// 액터 자체를 Destory하면 BlockUnit에서 사용할 수가 없음
-	Renderer->Destroy();
 }
