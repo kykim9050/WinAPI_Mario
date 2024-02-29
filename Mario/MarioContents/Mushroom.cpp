caa12b81645d118040d5227638527266c43e58ff
kky <kykim9050@naver.com>
2024-02-29 13:59:39 +0900

    김경윤 : Mushroom 클래스 추가

---
+++ Mario/MarioContents/Mushroom.cpp
@@ -0,0 +1,32 @@
+﻿#include "Mushroom.h"
+
+AMushroom::AMushroom()
+{
+}
+
+AMushroom::~AMushroom()
+{
+}
+
+void AMushroom::BeginPlay()
+{
+	AItemUnit::BeginPlay();
+
+	/*Renderer = CreateImageRenderer(static_cast<int>(EStageRenderOrder::Item));
+	Renderer->SetImage("Coin.png");
+
+	FVector ItemScale = Renderer->GetImage()->GetScale();
+
+	Renderer->SetTransform({ {0,0}, {ItemScale.iX() / UInGameValue::CoinImageXValue, ItemScale.iY() / UInGameValue::CoinImageYValue} });
+	Renderer->CreateAnimation("Rotating_Coin", "Coin.png", 0, 3, 0.05f, true);
+	Renderer->ChangeAnimation("Rotating_Coin");
+
+	StateChange(EActorState::Idle);*/
+}
+
+void AMushroom::Tick(float _DeltaTime)
+{
+	AItemUnit::Tick(_DeltaTime);
+}
+
+
