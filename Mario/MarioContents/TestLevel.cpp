20b307a4071dba70cf5cdf2f1803273098ada8ca
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-03-01 16:30:29 +0900

    김경윤 : 다시 Mushroom과 충돌시 정지 안되는 버그 해결

--- Mario/MarioContents/TestLevel.cpp
+++ Mario/MarioContents/TestLevel.cpp
@@ -38,7 +38,7 @@ void UTestLevel::BeginPlay()
 	ACoinBlock* CoinBlock1 = SpawnActor< ACoinBlock>(static_cast<int>(EActorType::Block));
 	//ACoinBlock* CoinBlock2 = SpawnActor< ACoinBlock>(static_cast<int>(EActorType::Block));
 	//ACoinBlock* CoinBlock3 = SpawnActor< ACoinBlock>(static_cast<int>(EActorType::Block));
-	//ACoinBlock* CoinBlock4 = SpawnActor< ACoinBlock>(static_cast<int>(EActorType::Block));
+	ACoinBlock* CoinBlock4 = SpawnActor< ACoinBlock>(static_cast<int>(EActorType::Block));
 	//AItemUnit* Coin = SpawnActor< AItemUnit>(static_cast<int>(EActorType::Item));
 
 	AGoomba* MonsterGoomba1 = SpawnActor<AGoomba>(static_cast<int>(EActorType::Monster));
@@ -61,7 +61,7 @@ void UTestLevel::BeginPlay()
 	CoinBlock1->SetActorLocation({ 250 + 48 * 4,450 });
 	//CoinBlock2->SetActorLocation({ 250 + 48 * 5,450 });
 	//CoinBlock3->SetActorLocation({ 250 + 48 * 6,450 });
-	//CoinBlock4->SetActorLocation({ 250 + 48 * 7,450 });
+	CoinBlock4->SetActorLocation({ 250 + 48 * 7,450 });
 
 	//Coin->SetActorLocation({ 250,400 });
 
