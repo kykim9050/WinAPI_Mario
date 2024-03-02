fda32e2bad64004d5feab049899d8b3d1c27acff
kykim9050 <kykim9050@naver.com>
2024-03-03 06:30:19 +0900

    김경윤 : FireFlower 랜더링 구현

--- Mario/MarioContents/FireFlower.h
+++ Mario/MarioContents/FireFlower.h
@@ -17,20 +17,20 @@ public:
 	AFireFlower& operator=(AFireFlower&& _Other) noexcept = delete;
 
 protected:
-	void CollisionCheck() override;
-	void CollisionStateChange(ECollisionState _CollisionState) override;
+	//void CollisionCheck() override;
+	//void CollisionStateChange(ECollisionState _CollisionState) override;
 
-	void FirstInit(float _DeltaTime) override;
-	void Idle(float _DeltaTime) override;
-	void Move(float _DeltaTime) override;
-	void Appear(float _DeltaTime) override;
+	//void FirstInit(float _DeltaTime) override;
+	//void Idle(float _DeltaTime) override;
+	//void Move(float _DeltaTime) override;
+	//void Appear(float _DeltaTime) override;
 
 
-	void IdleStart() override;
-	void AppearStart() override;
-	void MoveStart() override;
-	void ReleaseStart() override;
-	void GetHitStart() override;
+	//void IdleStart() override;
+	//void AppearStart() override;
+	//void MoveStart() override;
+	//void ReleaseStart() override;
+	//void GetHitStart() override;
 
 	void BeginPlay() override;
 	void Tick(float _DeltaTime) override;
