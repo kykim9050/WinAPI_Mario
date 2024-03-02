8151ba34a68b4a483b8ecf8dd9141f9c2c810ab7
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-03-03 01:13:27 +0900

    김경윤 : FireFlower 클래스 추가

---
+++ Mario/MarioContents/FireFlower.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "ItemUnit.h"
+
+
+// Ό³Έν :
+class AFireFlower : public AItemUnit
+{
+public:
+	// constrcuter destructer
+	AFireFlower();
+	~AFireFlower();
+
+	// delete Function
+	AFireFlower(const AFireFlower& _Other) = delete;
+	AFireFlower(AFireFlower&& _Other) noexcept = delete;
+	AFireFlower& operator=(const AFireFlower& _Other) = delete;
+	AFireFlower& operator=(AFireFlower&& _Other) noexcept = delete;
+
+protected:
+	void CollisionCheck() override;
+	void CollisionStateChange(ECollisionState _CollisionState) override;
+
+	void FirstInit(float _DeltaTime) override;
+	void Idle(float _DeltaTime) override;
+	void Move(float _DeltaTime) override;
+	void Appear(float _DeltaTime) override;
+
+
+	void IdleStart() override;
+	void AppearStart() override;
+	void MoveStart() override;
+	void ReleaseStart() override;
+	void GetHitStart() override;
+
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+
+};
+
