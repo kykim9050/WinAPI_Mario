2eb04b78ccd06e2a9b4b4e1e78910d4b2136edb7
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-03-03 17:03:06 +0900

    김경윤 : EndFlag Test 환경 구축

--- Mario/MarioContents/TestLevel.cpp
+++ Mario/MarioContents/TestLevel.cpp
@@ -12,7 +12,7 @@
 #include "BrickBlock.h"
 #include "CoinBlock.h"
 #include "ItemUnit.h"
-#include "StructureUnit.h"
+#include "EndFlag.h"
 
 UTestLevel::UTestLevel()
 {
@@ -30,7 +30,7 @@ void UTestLevel::BeginPlay()
 	UCollisionMap* CollisionMap = SpawnActor< UCollisionMap>(static_cast<int>(EActorType::Map));
 	APlayerMario* Mario = SpawnActor< APlayerMario>(static_cast<int>(EActorType::Player));
 	AUI* PlayerUI = SpawnActor<AUI>(static_cast<int>(EActorType::UI));
-	AStructureUnit* EndFlag = SpawnActor<AStructureUnit>(static_cast<int>(EActorType::Structure));
+	AEndFlag* EndFlag = SpawnActor<AEndFlag>(static_cast<int>(EActorType::Structure));
 
 	//ABrickBlock* BrickBlock1 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
 	//ABrickBlock* BrickBlock2 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
@@ -50,12 +50,12 @@ void UTestLevel::BeginPlay()
 	//AGoomba* MonsterGoomba2 = SpawnActor<AGoomba>(static_cast<int>(EActorType::Monster));
 	//AGoomba* MonsterGoomba3 = SpawnActor<AGoomba>(static_cast<int>(EActorType::Monster));
 	//AKoopaTroopa* MonsterKoopaTroopa1 = SpawnActor< AKoopaTroopa>(static_cast<int>(EActorType::Monster));
-	AKoopaTroopa* MonsterKoopaTroopa2 = SpawnActor< AKoopaTroopa>(static_cast<int>(EActorType::Monster));
+	//AKoopaTroopa* MonsterKoopaTroopa2 = SpawnActor< AKoopaTroopa>(static_cast<int>(EActorType::Monster));
 	//APiranhaPlant* MonsterPiranhaPlant1 = SpawnActor< APiranhaPlant>(static_cast<int>(EActorType::Monster));
 
 	Mario->SetActorLocation({ UInGameValue::MarioInitXPos, UInGameValue::MarioInitYPos });
 	PlayerUI->SetActorLocation({ UInGameValue::UIXScaleValue / 2 * UInGameValue::WindowSizeMulValue, UInGameValue::UIYScaleValue / 2 * UInGameValue::WindowSizeMulValue });
-	EndFlag->SetActorLocation({ UInGameValue::EndFlagXPos/*3176*/ * UInGameValue::WindowSizeMulValue, UInGameValue::EndFlagYPos/*124*/ * UInGameValue::WindowSizeMulValue });
+	EndFlag->SetActorLocation({ 200 * UInGameValue::WindowSizeMulValue, UInGameValue::EndFlagYPos/*124*/ * UInGameValue::WindowSizeMulValue });
 
 	int StandardYPos = 450;
 	int StandardXPos = 600;
@@ -76,7 +76,7 @@ void UTestLevel::BeginPlay()
 	//MonsterPiranhaPlant1->SetActorLocation({ 1875,550 });
 	//
 	//MonsterKoopaTroopa1->SetActorLocation({ 1000,500 });
-	MonsterKoopaTroopa2->SetActorLocation({ 1700,500 });
+	//MonsterKoopaTroopa2->SetActorLocation({ 1700,500 });
 	//
 	//MonsterGoomba1->SetActorLocation({ 600, 500 });
 	//MonsterGoomba2->SetActorLocation({ 700, 500 });
