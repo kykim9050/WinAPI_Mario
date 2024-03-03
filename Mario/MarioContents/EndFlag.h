61758e8ce84bf0c32e6fd504d9f0769643450962
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-03-03 15:45:27 +0900

    김경윤 : EndFlag 클래스 생성

---
+++ Mario/MarioContents/EndFlag.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "StructureUnit.h"
+
+// Ό³Έν :
+class AEndFlag : public AStructureUnit
+{
+public:
+	// constrcuter destructer
+	AEndFlag();
+	~AEndFlag();
+
+	// delete Function
+	AEndFlag(const AEndFlag& _Other) = delete;
+	AEndFlag(AEndFlag&& _Other) noexcept = delete;
+	AEndFlag& operator=(const AEndFlag& _Other) = delete;
+	AEndFlag& operator=(AEndFlag&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+
+};
+
