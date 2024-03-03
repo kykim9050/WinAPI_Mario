ee8ce8c291108649d5fc3ef6b9491490e310f3ec
kykim9050 <kykim9050@naver.com>
2024-03-03 15:02:28 +0900

    김경윤 : StructureUnit 클래스 생성

---
+++ Mario/MarioContents/StructureUnit.cpp
@@ -0,0 +1,19 @@
+#include "StructureUnit.h"
+
+AStructureUnit::AStructureUnit()
+{
+}
+
+AStructureUnit::~AStructureUnit()
+{
+}
+
+void AStructureUnit::BeginPlay()
+{
+	UStateUnit::BeginPlay();
+}
+
+void AStructureUnit::Tick(float _DeltaTime)
+{
+	UStateUnit::Tick(_DeltaTime);
+}
