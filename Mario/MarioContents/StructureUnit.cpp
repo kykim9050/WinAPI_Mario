1b58f859e04bd6ce2208b9ac1d5446607558b7a8
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-03-03 15:31:57 +0900

    김경윤 : 이미지 리소스 추가

--- Mario/MarioContents/StructureUnit.cpp
+++ Mario/MarioContents/StructureUnit.cpp
@@ -13,12 +13,12 @@ void AStructureUnit::BeginPlay()
 	UStateUnit::BeginPlay();
 
 	Renderer = CreateImageRenderer(static_cast<int>(EStageRenderOrder::Item));
-	Renderer->SetImage("Flag.png");
+	Renderer->SetImage("EndFlag.png");
 	
 	FVector EndFlagScale = Renderer->GetImage()->GetScale();
 
 	//Renderer->SetTransform({ {0,0}, {GoombaScale.iX() / UInGameValue::GoombaImageXValue * UInGameValue::WindowSizeMulValue, GoombaScale.iY() / UInGameValue::GoombaImageYValue * UInGameValue::WindowSizeMulValue} });
-	Renderer->SetTransform({ {0,0}, {EndFlagScale.iX() / 1.5f, EndFlagScale.iY() / 1.5f} });
+	Renderer->SetTransform({ {0,0}, {EndFlagScale.iX() * UInGameValue::WindowSizeMulValue, EndFlagScale.iY() * UInGameValue::WindowSizeMulValue} });
 	//Renderer->CreateAnimation("Goomba_Move", "Goomba.png", 0, 1, 0.2f, true);
 	//Renderer->CreateAnimation("Goomba_Dead", "Goomba.png", 2, 2, 0.1f, true);
 	//Renderer->ChangeAnimation("Goomba_Move");
