83a44e4968450d061105dda8f23c58f41e6d825e
kykim9050 <kykim9050@naver.com>
2024-03-03 15:45:27 +0900

    김경윤 : EndFlag 클래스 생성

---
+++ Mario/MarioContents/EndFlag.cpp
@@ -0,0 +1,19 @@
+#include "EndFlag.h"
+
+AEndFlag::AEndFlag()
+{
+}
+
+AEndFlag::~AEndFlag()
+{
+}
+
+void AEndFlag::BeginPlay()
+{
+	AStructureUnit::BeginPlay();
+}
+
+void AEndFlag::Tick(float _DeltaTime)
+{
+	AStructureUnit::Tick(_DeltaTime);
+}
