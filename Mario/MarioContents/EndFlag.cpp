0032c39b29fe9e5ad983f2a8c5e837fdf3fd7aed
kykim9050 <kykim9050@naver.com>
2024-03-03 17:03:06 +0900

    김경윤 : EndFlag Test 환경 구축

--- Mario/MarioContents/EndFlag.cpp
+++ Mario/MarioContents/EndFlag.cpp
@@ -11,6 +11,21 @@ AEndFlag::~AEndFlag()
 void AEndFlag::BeginPlay()
 {
 	AStructureUnit::BeginPlay();
+
+	Renderer = CreateImageRenderer(static_cast<int>(EStageRenderOrder::Item));
+	Renderer->SetImage("EndFlag.png");
+
+	FVector EndFlagScale = Renderer->GetImage()->GetScale();
+
+	Renderer->SetTransform({ {0,0}, {EndFlagScale.iX() * UInGameValue::WindowSizeMulValue, EndFlagScale.iY() * UInGameValue::WindowSizeMulValue} });
+
+	
+	//BodyCollision = CreateCollision(ECollisionOrder::Monster);
+	//BodyCollision->SetTransform({ { 0,0 }, { UInGameValue::GoombaBodyCollisionScaleX, UInGameValue::GoombaBodyCollisionScaleY } });
+	//BodyCollision->SetPosition({ 0, -(BodyCollision->GetTransform().GetScale().ihY()) });
+	//BodyCollision->SetColType(ECollisionType::Rect);
+
+	//SetActorState(EActorState::Move);
 }
 
 void AEndFlag::Tick(float _DeltaTime)
--- Mario/MarioContents/StructureUnit.cpp
+++ Mario/MarioContents/StructureUnit.cpp
@@ -11,25 +11,6 @@ AStructureUnit::~AStructureUnit()
 void AStructureUnit::BeginPlay()
 {
 	UStateUnit::BeginPlay();
-
-	Renderer = CreateImageRenderer(static_cast<int>(EStageRenderOrder::Item));
-	Renderer->SetImage("EndFlag.png");
-	
-	FVector EndFlagScale = Renderer->GetImage()->GetScale();
-
-	Renderer->SetTransform({ {0,0}, {EndFlagScale.iX() * UInGameValue::WindowSizeMulValue, EndFlagScale.iY() * UInGameValue::WindowSizeMulValue} });
-	
-	//Renderer->CreateAnimation("Goomba_Move", "Goomba.png", 0, 1, 0.2f, true);
-	//Renderer->CreateAnimation("Goomba_Dead", "Goomba.png", 2, 2, 0.1f, true);
-	//Renderer->ChangeAnimation("Goomba_Move");
-
-
-	//BodyCollision = CreateCollision(ECollisionOrder::Monster);
-	//BodyCollision->SetTransform({ { 0,0 }, { UInGameValue::GoombaBodyCollisionScaleX, UInGameValue::GoombaBodyCollisionScaleY } });
-	//BodyCollision->SetPosition({ 0, -(BodyCollision->GetTransform().GetScale().ihY()) });
-	//BodyCollision->SetColType(ECollisionType::Rect);
-
-	//SetActorState(EActorState::Move);
 }
 
 void AStructureUnit::Tick(float _DeltaTime)
