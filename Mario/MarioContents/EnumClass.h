c3ed9a7790076596a09e9f75526ee1a43312e511
kky <kykim9050@naver.com>
2024-03-04 15:27:53 +0900

    김경윤 : UI창에 숫자 출력 구현

--- Mario/MarioContents/EnumClass.h
+++ Mario/MarioContents/EnumClass.h
@@ -33,6 +33,7 @@ enum class EStageRenderOrder
 	Monster,
 	Mario,
 	UI,
+	UIComponent,
 };
 
 /// <summary>
--- Mario/MarioContents/UI.h
+++ Mario/MarioContents/UI.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <EngineCore/Actor.h>
 
+
 // 설명 : UI는 플레이어와 독립되었지만 플레이어의 점수를 관여하고 있다.
 // 플레이어는 사라지더라도 해당 UI점수는 남아있는 것으로 하고 새로운 레벨이 생성될 때 해당 UI점수와 랜더를 전달하도록 한다.
 class AUI : public AActor
@@ -25,6 +26,8 @@ protected:
 private:
 	UImageRenderer* PlayerUIRenderer = nullptr;
 	
+
+	UImageRenderer* TimeRenderer = nullptr;
 	int TimeCount = 11;
 	float TimeInterval = 1.0f;
 
