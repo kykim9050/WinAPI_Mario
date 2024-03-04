c8c7c40962b5e76c3389d0ae63a0b531f6f299fe
Kyungyoon K <kyungyoon.kim@sheco.co>
2024-03-04 21:57:25 +0900

    김경윤 : Stage1-1 블럭위치 지정

--- Mario/MarioContents/1_1StageLevel.cpp
+++ Mario/MarioContents/1_1StageLevel.cpp
@@ -41,12 +41,26 @@ void U1_1StageLevel::BeginPlay()
 	ABrickBlock* BrickBlock1 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
 	ABrickBlock* BrickBlock2 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
 	ABrickBlock* BrickBlock3 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock4 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock5 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock6 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock7 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock8 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock9 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock10 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock11 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock12 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock13 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock14 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock15 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
+	ABrickBlock* BrickBlock16 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
 
 
 	ACoinBlock* CoinBlock1 = SpawnCoinBlock(EActorType::Block, EItemType::Coin);
 	ACoinBlock* CoinBlock2 = SpawnCoinBlock(EActorType::Block, EItemType::Item);
 	ACoinBlock* CoinBlock3 = SpawnCoinBlock(EActorType::Block, EItemType::Coin);
 	ACoinBlock* CoinBlock4 = SpawnCoinBlock(EActorType::Block, EItemType::Coin);
+	ACoinBlock* CoinBlock5 = SpawnCoinBlock(EActorType::Block, EItemType::Item);
 
 	int StandardYPos = 456;
 	int SecondYPos = 264;
@@ -58,6 +72,23 @@ void U1_1StageLevel::BeginPlay()
 	CoinBlock3->SetActorLocation({ 984 + 48 * 3,StandardYPos });
 	BrickBlock3->SetActorLocation({ 984 + 48 * 4,StandardYPos });
 	CoinBlock4->SetActorLocation({ 984 + 48 * 2,SecondYPos });
+
+
+	BrickBlock4->SetActorLocation({ 3720, StandardYPos });
+	CoinBlock5->SetActorLocation({ 3720 + 48, StandardYPos });
+	BrickBlock5->SetActorLocation({ 3720 + 48 *2, StandardYPos });
+	BrickBlock6->SetActorLocation({ 3720 + 48 * 3, SecondYPos });
+	BrickBlock7->SetActorLocation({ 3720 + 48 * 4, SecondYPos });
+	BrickBlock8->SetActorLocation({ 3720 + 48 * 5, SecondYPos });
+	BrickBlock9->SetActorLocation({ 3720 + 48 * 6, SecondYPos });
+	BrickBlock10->SetActorLocation({ 3720 + 48 * 7, SecondYPos });
+	BrickBlock11->SetActorLocation({ 3720 + 48 * 8, SecondYPos });
+	BrickBlock12->SetActorLocation({ 3720 + 48 * 9, SecondYPos });
+	BrickBlock13->SetActorLocation({ 3720 + 48 * 10, SecondYPos });
+
+	BrickBlock14->SetActorLocation({ 3720 + 48 * 14, SecondYPos });
+	BrickBlock15->SetActorLocation({ 3720 + 48 * 15, SecondYPos });
+	BrickBlock16->SetActorLocation({ 3720 + 48 * 16, SecondYPos });
 }
 
 void U1_1StageLevel::Tick(float _DeltaTime)
