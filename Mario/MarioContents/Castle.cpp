1a1a4e9d91b858da76814712ff6d06509a10eb94
kykim9050 <kykim9050@naver.com>
2024-03-04 11:33:17 +0900

    김경윤 : Castle 클래스 생성

---
+++ Mario/MarioContents/Castle.cpp
@@ -0,0 +1,24 @@
+﻿#include "Castle.h"
+
+ACastle::ACastle()
+{
+}
+
+ACastle::~ACastle()
+{
+}
+
+void ACastle::BeginPlay()
+{
+	AStructureUnit::BeginPlay();
+}
+
+void ACastle::Tick(float _DeltaTime)
+{
+	AStructureUnit::Tick(_DeltaTime);
+}
+
+void ACastle::CollisionCheck()
+{
+	AStructureUnit::CollisionCheck();
+}
---
+++ Mario/MarioContents/Castle.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "StructureUnit.h"
+
+// Ό³Έν :
+class ACastle : public AStructureUnit
+{
+public:
+	// constrcuter destructer
+	ACastle();
+	~ACastle();
+
+	// delete Function
+	ACastle(const ACastle& _Other) = delete;
+	ACastle(ACastle&& _Other) noexcept = delete;
+	ACastle& operator=(const ACastle& _Other) = delete;
+	ACastle& operator=(ACastle&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+	void CollisionCheck() override;
+
+
+private:
+
+};
+
