b7097881bccbc152f37ae75cf9e630fcaab5b193
kky <kykim9050@naver.com>
2024-03-04 11:41:25 +0900

    김경윤 : Castle 랜더링 구현

--- Mario/MarioContents/Castle.cpp
+++ Mario/MarioContents/Castle.cpp
@@ -11,6 +11,17 @@ ACastle::~ACastle()
 void ACastle::BeginPlay()
 {
 	AStructureUnit::BeginPlay();
+
+	Renderer = CreateImageRenderer(static_cast<int>(EStageRenderOrder::BackGrundStructure));
+	Renderer->SetImage("MiniCastle.png");
+
+	FVector CastleScale = Renderer->GetImage()->GetScale();
+
+	Renderer->SetTransform({ {0,0}, {CastleScale.iX() * UInGameValue::WindowSizeMulValue, CastleScale.iY() * UInGameValue::WindowSizeMulValue} });
+
+	//BodyCollision = CreateCollision(ECollisionOrder::EndFlagLine);
+	//BodyCollision->SetTransform({ { 0,0 }, { UInGameValue::EndFlagCollisionXScale, UInGameValue::EndFlagCollisionYScale} });
+	//BodyCollision->SetColType(ECollisionType::Rect);
 }
 
 void ACastle::Tick(float _DeltaTime)
