1fac5b8b4418b04fcb7f5c4450acb9d763612545
kykim9050 <kykim9050@naver.com>
2024-03-05 13:10:20 +0900

    김경윤 : GetMainPlayer로 플레이어를 알 수 있는 MainPlayer 클래스 생성

---
+++ Mario/MarioContents/MainPlayer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+class APlayerMario;
+
+// Ό³Έν :
+class UMainPlayer
+{
+public:
+	// constrcuter destructer
+	UMainPlayer();
+	~UMainPlayer();
+
+	// delete Function
+	UMainPlayer(const UMainPlayer& _Other) = delete;
+	UMainPlayer(UMainPlayer&& _Other) noexcept = delete;
+	UMainPlayer& operator=(const UMainPlayer& _Other) = delete;
+	UMainPlayer& operator=(UMainPlayer&& _Other) noexcept = delete;
+
+protected:
+	APlayerMario* Player = nullptr;
+
+private:
+
+};
+
