3f5aea9cd3500d359574a2a13f1b9b7a039a0092
kykim9050 <kykim9050@naver.com>
2024-03-05 13:44:50 +0900

    김경윤 : 코드 수정

--- Mario/MarioContents/TestLevel.cpp
+++ Mario/MarioContents/TestLevel.cpp
@@ -36,49 +36,30 @@ void UTestLevel::BeginPlay()
 
 	ABrickBlock* BrickBlock1 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
 	ABrickBlock* BrickBlock2 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
-	//ABrickBlock* BrickBlock3 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
-	//ABrickBlock* BrickBlock4 = SpawnActor< ABrickBlock>(static_cast<int>(EActorType::Block));
 
-	//ACoinBlock* CoinBlock1 = SpawnCoinBlock(EActorType::Block, EItemType::Coin);
 	ACoinBlock* CoinBlock5 = SpawnCoinBlock(EActorType::Block, EItemType::Item);
 	ACoinBlock* CoinBlock6 = SpawnCoinBlock(EActorType::Block, EItemType::Item);
-	//ACoinBlock* CoinBlock7 = SpawnCoinBlock(EActorType::Block, EItemType::Coin);
 
-	//AGoomba* MonsterGoomba1 = SpawnActor<AGoomba>(static_cast<int>(EActorType::Monster));
-	//AGoomba* MonsterGoomba2 = SpawnActor<AGoomba>(static_cast<int>(EActorType::Monster));
-	//AGoomba* MonsterGoomba3 = SpawnActor<AGoomba>(static_cast<int>(EActorType::Monster));
-	//AKoopaTroopa* MonsterKoopaTroopa1 = SpawnActor< AKoopaTroopa>(static_cast<int>(EActorType::Monster));
-	//AKoopaTroopa* MonsterKoopaTroopa2 = SpawnActor< AKoopaTroopa>(static_cast<int>(EActorType::Monster));
-	//APiranhaPlant* MonsterPiranhaPlant1 = SpawnActor< APiranhaPlant>(static_cast<int>(EActorType::Monster));
+	AGoomba* Goomba1 = SpawnActor< AGoomba>(static_cast<int>(EActorType::Monster));
+
+
 
 	Mario->SetActorLocation({ UInGameValue::MarioInitXPos, UInGameValue::MarioInitYPos });
 	PlayerUI->SetActorLocation({ UInGameValue::UIXScaleValue / 2 * UInGameValue::WindowSizeMulValue, UInGameValue::UIYScaleValue / 2 * UInGameValue::WindowSizeMulValue });
 	EndPointCastle->SetActorLocation({ UInGameValue::EndPointCastleXPos, UInGameValue::EndPointCastleYPos });
 	EndFlag->SetActorLocation({ UInGameValue::EndFlagXPos, UInGameValue::EndFlagYPos});
-	//EndPointCastle->SetActorLocation({ UInGameValue::EndPointCastleXPos, UInGameValue::EndPointCastleXPos});
-
 
 	int StandardYPos = 450;
 	int StandardXPos = 600;
 
 	BrickBlock1->SetActorLocation({ StandardXPos + 48*20,StandardYPos });
 	BrickBlock2->SetActorLocation({ StandardXPos + 48*21,StandardYPos });
-	//BrickBlock3->SetActorLocation({ StandardXPos + 48 * 2,StandardYPos });
-	//BrickBlock4->SetActorLocation({ StandardXPos + 48 * 3,StandardYPos });
 	
-	//CoinBlock1->SetActorLocation({ 250 + 48 * 4,StandardYPos });
 	CoinBlock5->SetActorLocation({ 250 + 48 * 8,StandardYPos });
 	CoinBlock6->SetActorLocation({ 250 + 48 * 12,StandardYPos });
-	//CoinBlock7->SetActorLocation({ 250 + 48 * 16,StandardYPos });
 
-	//MonsterPiranhaPlant1->SetActorLocation({ 1875,550 });
-	//
-	//MonsterKoopaTroopa1->SetActorLocation({ 1000,500 });
-	//MonsterKoopaTroopa2->SetActorLocation({ 1700,500 });
-	//
-	//MonsterGoomba1->SetActorLocation({ 600, 500 });
-	//MonsterGoomba2->SetActorLocation({ 700, 500 });
-	//MonsterGoomba3->SetActorLocation({ 900, 500 });
+	Goomba1->SetActorLocation({ 250 + 48 * 2,StandardYPos });
+
 }
 
 void UTestLevel::Tick(float _DeltaTime)
