5d93d87c9a81fbe1410cdbec9a0b417113482341
kykim9050 <kykim9050@naver.com>
2024-03-06 11:47:20 +0900

    김경윤 : TestLevel 구조 변경

--- Mario/MarioContents/ContentsCore.cpp
+++ Mario/MarioContents/ContentsCore.cpp
@@ -71,11 +71,11 @@ void UContentsCore::BeginPlay()
 	//CreateLevel<U1_1StageLevel>("1_1StageLevel");
 	//ChangeLevel("1_1StageLevel");
 
-	/*CreateLevel<UTestLevel>("1-1");
-	ChangeLevel("1-1");*/
+	CreateLevel<UTestLevel>("1-1");
+	ChangeLevel("1-1");
 
-	CreateLevel<U1_4StageLevel>("1-4");
-	ChangeLevel("1-4");
+	/*CreateLevel<U1_4StageLevel>("1-4");
+	ChangeLevel("1-4");*/
 
 
 }
