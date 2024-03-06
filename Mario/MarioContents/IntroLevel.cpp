0219a77ff2ba3560e76985cc71e7f43c4844c531
kykim9050 <kykim9050@naver.com>
2024-03-06 13:53:10 +0900

    김경윤 : IntroLevel 클래스 생성

---
+++ Mario/MarioContents/IntroLevel.cpp
@@ -0,0 +1,29 @@
+﻿#include "IntroLevel.h"
+
+UIntroLevel::UIntroLevel()
+{
+}
+
+UIntroLevel::~UIntroLevel()
+{
+}
+
+void UIntroLevel::BeginPlay()
+{
+	ULevel::BeginPlay();
+}
+
+void UIntroLevel::Tick(float _DeltaTime)
+{
+	ULevel::Tick(_DeltaTime);
+}
+
+void UIntroLevel::LevelStart(ULevel* _PrevLevel)
+{
+	ULevel::LevelStart(_PrevLevel);
+}
+
+void UIntroLevel::LevelEnd(ULevel* _NextLevel)
+{
+	ULevel::LevelEnd(_NextLevel);
+}
---
+++ Mario/MarioContents/IntroLevel.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <EngineCore/Level.h>
+
+// Ό³Έν :
+class UIntroLevel : public ULevel
+{
+public:
+	// constrcuter destructer
+	UIntroLevel();
+	~UIntroLevel();
+
+	// delete Function
+	UIntroLevel(const UIntroLevel& _Other) = delete;
+	UIntroLevel(UIntroLevel&& _Other) noexcept = delete;
+	UIntroLevel& operator=(const UIntroLevel& _Other) = delete;
+	UIntroLevel& operator=(UIntroLevel&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+	void LevelStart(ULevel* _PrevLevel) override;
+	void LevelEnd(ULevel* _NextLevel) override;
+
+private:
+
+};
+
