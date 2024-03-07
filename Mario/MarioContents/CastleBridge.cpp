7d22736ac9d9e00c8c2b9504f0305cf0c5865f54
kky <kykim9050@naver.com>
2024-03-07 14:33:22 +0900

    김경윤 : CastleBridge 클래스 구현

---
+++ Mario/MarioContents/CastleBridge.cpp
@@ -0,0 +1,20 @@
+﻿#include "CastleBridge.h"
+
+ACastleBridge::ACastleBridge()
+{
+}
+
+ACastleBridge::~ACastleBridge()
+{
+}
+
+void ACastleBridge::BeginPlay()
+{
+	ABlockUnit::BeginPlay();
+}
+
+void ACastleBridge::Tick(float _DeltaTime)
+{
+	ABlockUnit::Tick(_DeltaTime);
+}
+
