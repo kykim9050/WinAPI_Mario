34ca0c4a1596f83f774ef5cee0f59b54ac17dab1
kky <kykim9050@naver.com>
2024-03-07 16:07:59 +0900

    김경윤 : CastleBridge 클래스를 활용해서 BridgeBlock으로 다리를 구현 (해당 클래스를 통해서 블럭들의상태를 관리할 것)

---
+++ Mario/MarioContents/CastleBridge.cpp
@@ -0,0 +1,28 @@
+﻿#include "CastleBridge.h"
+#include "BridgeBlock.h"
+
+ACastleBridge::ACastleBridge()
+{
+
+}
+
+ACastleBridge::~ACastleBridge()
+{
+}
+
+void ACastleBridge::BeginPlay()
+{
+	AStructureUnit::BeginPlay();
+
+	for (int i = 0; i < UInGameValue::BridgeBlockAmount; i++)
+	{
+		ABridgeBlock* BridgeBlock = GetWorld()->SpawnActor<ABridgeBlock>(static_cast<int>(EActorType::Block));
+		BridgeBlock->SetActorLocation({ UInGameValue::BridgeStartXPos + UInGameValue::BTBDistance * i,  UInGameValue::BridgeStartYPos });
+		BridgeBlocks.push_front(BridgeBlock);
+	}
+}
+
+void ACastleBridge::Tick(float _DeltaTime)
+{
+	AStructureUnit::Tick(_DeltaTime);
+}
