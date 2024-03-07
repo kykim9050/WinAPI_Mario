24f98342fecd30a59d715ca0012727b0fe5ed8f0
kykim9050 <kykim9050@naver.com>
2024-03-07 13:19:05 +0900

    김경윤 : Koopa클래스 생성

---
+++ Mario/MarioContents/Koopa.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "MonsterUnit.h"
+
+// Ό³Έν :
+class AKoopa : public AMonsterUnit
+{
+public:
+	// constrcuter destructer
+	AKoopa();
+	~AKoopa();
+
+	// delete Function
+	AKoopa(const AKoopa& _Other) = delete;
+	AKoopa(AKoopa&& _Other) noexcept = delete;
+	AKoopa& operator=(const AKoopa& _Other) = delete;
+	AKoopa& operator=(AKoopa&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+
+};
+
