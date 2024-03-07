7a89def2c2217c1c068cfd28c02b2cc11b038bce
kky <kykim9050@naver.com>
2024-03-07 13:19:05 +0900

    김경윤 : Koopa클래스 생성

---
+++ Mario/MarioContents/Koopa.cpp
@@ -0,0 +1,19 @@
+﻿#include "Koopa.h"
+
+AKoopa::AKoopa()
+{
+}
+
+AKoopa::~AKoopa()
+{
+}
+
+void AKoopa::BeginPlay()
+{
+	AMonsterUnit::BeginPlay();
+}
+
+void AKoopa::Tick(float _DeltaTime)
+{
+	AMonsterUnit::Tick(_DeltaTime);
+}
