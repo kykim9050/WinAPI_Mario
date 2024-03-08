a469a091d2ab0f93f2736aec3df0eab2b5517529
kykim9050 <kykim9050@naver.com>
2024-03-08 18:08:32 +0900

    김경윤 : FootBoard 클래스 추가

---
+++ Mario/MarioContents/FootBoard.cpp
@@ -0,0 +1,19 @@
+﻿#include "FootBoard.h"
+
+AFootBoard::AFootBoard()
+{
+}
+
+AFootBoard::~AFootBoard()
+{
+}
+
+void AFootBoard::BeginPlay()
+{
+	UStateUnit::BeginPlay();
+}
+
+void AFootBoard::Tick(float _DeltaTime)
+{
+	UStateUnit::Tick(_DeltaTime);
+}
