66ce014fa08201dfde4b72cb33b1971f651d82f8
kykim9050 <kykim9050@naver.com>
2024-03-08 14:54:13 +0900

    김경윤 : KoopaBullet 클래스 추가

--- Mario/MarioContents/BulletUnit.cpp
+++ Mario/MarioContents/BulletUnit.cpp
@@ -11,16 +11,6 @@ ABulletUnit::~ABulletUnit()
 void ABulletUnit::BeginPlay()
 {
 	UStateUnit::BeginPlay();
-
-	Renderer = CreateImageRenderer(static_cast<int>(EStageRenderOrder::Bullet));
-	Renderer->SetImage("KoopaFire_Left.png");
-
-	FVector KoopaFireScale = Renderer->GetImage()->GetScale();
-
-	Renderer->SetTransform({ {0,0}, {KoopaFireScale.iX() / UInGameValue::KoopaFireImgXValue * UInGameValue::WindowSizeMulValue, KoopaFireScale.iY() / UInGameValue::KoopaFireImgYValue * UInGameValue::WindowSizeMulValue} });
-	Renderer->CreateAnimation("Firing_Left", "KoopaFire_Left.png", 0, 1, 0.1f, true);
-	Renderer->CreateAnimation("Firing_Right", "KoopaFire_Right.png", 0, 1, 0.1f, true);
-	Renderer->ChangeAnimation("Firing_Left");
 }
 
 void ABulletUnit::Tick(float _DeltaTime)
---
+++ Mario/MarioContents/KoopaBullet.cpp
@@ -0,0 +1,31 @@
+﻿#include "KoopaBullet.h"
+
+AKoopaBullet::AKoopaBullet()
+{
+}
+
+AKoopaBullet::~AKoopaBullet()
+{
+}
+
+void AKoopaBullet::BeginPlay()
+{
+	ABulletUnit::BeginPlay();
+
+	Renderer = CreateImageRenderer(static_cast<int>(EStageRenderOrder::Bullet));
+	Renderer->SetImage("KoopaFire_Left.png");
+
+	FVector KoopaFireScale = Renderer->GetImage()->GetScale();
+
+	Renderer->SetTransform({ {0,0}, {KoopaFireScale.iX() / UInGameValue::KoopaFireImgXValue * UInGameValue::WindowSizeMulValue, KoopaFireScale.iY() / UInGameValue::KoopaFireImgYValue * UInGameValue::WindowSizeMulValue} });
+	Renderer->CreateAnimation("Firing_Left", "KoopaFire_Left.png", 0, 1, 0.1f, true);
+	Renderer->CreateAnimation("Firing_Right", "KoopaFire_Right.png", 0, 1, 0.1f, true);
+	Renderer->ChangeAnimation("Firing_Left");
+}
+
+void AKoopaBullet::Tick(float _DeltaTime)
+{
+	ABulletUnit::Tick(_DeltaTime);
+}
+
+
