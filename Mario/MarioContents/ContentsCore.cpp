c369f9e476079776c8e750f996fcda4bf663373e
kykim9050 <kykim9050@naver.com>
2024-03-08 18:04:03 +0900

    김경윤 : FootBoard 이미지 리소스 추가

--- Mario/MarioContents/ContentsCore.cpp
+++ Mario/MarioContents/ContentsCore.cpp
@@ -72,11 +72,11 @@ void UContentsCore::BeginPlay()
 	//CreateLevel<UIntroLevel>("IntroLevel");
 	//CreateLevel<UEndingLevel>("EndingLevel");
 	
-	CreateLevel<U1_1StageLevel>("1-1");
-	ChangeLevel("1-1");
+	//CreateLevel<U1_1StageLevel>("1-1");
+	//ChangeLevel("1-1");
 
-	//CreateLevel<UTestLevel>("1-4");
-	//ChangeLevel("1-4");
+	CreateLevel<UTestLevel>("1-4");
+	ChangeLevel("1-4");
 
 	//CreateLevel<U1_4StageLevel>("1-4");
 	//ChangeLevel("1-4");
