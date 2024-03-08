0700221f599957818005c9e1f289f81ac6dc52cb
kykim9050 <kykim9050@naver.com>
2024-03-08 18:25:12 +0900

    김경윤 : FootBoard - Collision 구현

--- Mario/MarioContents/FootBoard.cpp
+++ Mario/MarioContents/FootBoard.cpp
@@ -20,10 +20,9 @@ void AFootBoard::BeginPlay()
 	Renderer->SetTransform({ {0,0}, {FootBoardScale.iX() * UInGameValue::WindowSizeMulValue, FootBoardScale.iY() * UInGameValue::WindowSizeMulValue} });
 
 
-	//BodyCollision = CreateCollision(ECollisionOrder::Monster);
-	//BodyCollision->SetTransform({ { 0,0 }, { UInGameValue::GoombaBodyCollisionScaleX, UInGameValue::GoombaBodyCollisionScaleY } });
-	//BodyCollision->SetPosition({ 0, -(BodyCollision->GetTransform().GetScale().ihY()) });
-	//BodyCollision->SetColType(ECollisionType::Rect);
+	BodyCollision = CreateCollision(ECollisionOrder::BlockTop);
+	BodyCollision->SetTransform({ { 0,0 }, { FootBoardScale.iX() * UInGameValue::WindowSizeMulValue, FootBoardScale.iY() * UInGameValue::WindowSizeMulValue } });
+	BodyCollision->SetColType(ECollisionType::Rect);
 
 	//SetActorState(EActorState::Idle);
 }
