926d777eecd585d04657785060e8c1a625a23c92
kykim9050 <kykim9050@naver.com>
2024-03-11 10:28:25 +0900

    김경윤 : RotatingFire 클래스 추가

---
+++ Mario/MarioContents/RotatingFire.cpp
@@ -0,0 +1,19 @@
+﻿#include "RotatingFire.h"
+
+ARotatingFire::ARotatingFire()
+{
+}
+
+ARotatingFire::~ARotatingFire()
+{
+}
+
+void ARotatingFire::BeginPlay()
+{
+	AMovingObjectUnit::BeginPlay();
+}
+
+void ARotatingFire::Tick(float _DeltaTime)
+{
+	AMovingObjectUnit::Tick(_DeltaTime);
+}
