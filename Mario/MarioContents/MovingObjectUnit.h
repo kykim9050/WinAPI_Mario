74320a9d5ad54b0fc65fa41ad183dd872a3dfd05
kky <kykim9050@naver.com>
2024-03-11 10:18:59 +0900

    김경윤 : MovingObjectUnit 클래스 생성

---
+++ Mario/MarioContents/MovingObjectUnit.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "StateUnit.h"
+#include "MainPlayer.h"
+
+// Ό³Έν :
+class AMovingObjectUnit : public UStateUnit, public UMainPlayer
+{
+public:
+	// constrcuter destructer
+	AMovingObjectUnit();
+	~AMovingObjectUnit();
+
+	// delete Function
+	AMovingObjectUnit(const AMovingObjectUnit& _Other) = delete;
+	AMovingObjectUnit(AMovingObjectUnit&& _Other) noexcept = delete;
+	AMovingObjectUnit& operator=(const AMovingObjectUnit& _Other) = delete;
+	AMovingObjectUnit& operator=(AMovingObjectUnit&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+	virtual void StateUpdate(float _DeltaTime);
+	virtual void StateChange(EActorState _ActorState);
+
+	virtual void MoveStart() {};
+	virtual void Idle(float _Deltatime) {};
+	virtual void Move(float _DeltaTime) {};
+
+	FVector InitPos = FVector::Zero;
+
+private:
+
+};
+
