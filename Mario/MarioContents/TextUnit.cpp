b4facfadedecf6e14443f406b7bcf92d4cc4be8c
kykim9050 <kykim9050@naver.com>
2024-03-12 14:49:24 +0900

    김경윤 : TextUnit 클래스 추가

---
+++ Mario/MarioContents/TextUnit.cpp
@@ -0,0 +1,20 @@
+﻿#include "TextUnit.h"
+
+ATextUnit::ATextUnit()
+{
+}
+
+ATextUnit::~ATextUnit()
+{
+}
+
+void ATextUnit::BeginPlay()
+{
+	AActor::BeginPlay();
+}
+
+void ATextUnit::Tick(float _DeltaTime)
+{
+	AActor::Tick(_DeltaTime);
+}
+
