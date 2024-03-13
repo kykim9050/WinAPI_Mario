59ff6b8c850fcaf48bc62d0c46f23130ba20f1f4
kykim9050 <kykim9050@naver.com>
2024-03-13 18:37:19 +0900

    김경윤 : PlayerMario - 공주를 만났을때 엔딩 음악 출력

--- Mario/MarioContents/ContentsCore.cpp
+++ Mario/MarioContents/ContentsCore.cpp
@@ -74,14 +74,14 @@ void UContentsCore::BeginPlay()
 		UEngineSound::Load(File.GetFullPath());
 	}
 
-	CreateLevel<UTestLevel>("1-1");
-	ChangeLevel("1-1");
+	//CreateLevel<UTestLevel>("1-1");
+	//ChangeLevel("1-1");
 
 	//CreateLevel<UTitleLevel>("TitleLevel");
 	//ChangeLevel("TitleLevel");
 	
-	//CreateLevel<U1_4StageLevel>("1-4");
-	//ChangeLevel("1-4");
+	CreateLevel<U1_4StageLevel>("1-4");
+	ChangeLevel("1-4");
 
 
 }
