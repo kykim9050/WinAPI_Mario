dacbd932ac9e01f7b9385b923dd34683071e6b57
kky <kykim9050@naver.com>
2024-03-14 11:54:07 +0900

    김경윤 : GameOverLevel 생성 - 플레이어 Life 전부 소진시 GameOverLevel로 이동

---
+++ Mario/MarioContents/GameOverLevel.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <EngineCore/Level.h>
+
+// Ό³Έν :
+class UGameOverLevel : public ULevel
+{
+public:
+	// constrcuter destructer
+	UGameOverLevel();
+	~UGameOverLevel();
+
+	// delete Function
+	UGameOverLevel(const UGameOverLevel& _Other) = delete;
+	UGameOverLevel(UGameOverLevel&& _Other) noexcept = delete;
+	UGameOverLevel& operator=(const UGameOverLevel& _Other) = delete;
+	UGameOverLevel& operator=(UGameOverLevel&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+
+};
+
