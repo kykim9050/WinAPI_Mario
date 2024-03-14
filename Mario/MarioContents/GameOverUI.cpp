8df388e3bb6ac1631cdc3c109dad19351a3e0f37
kky <kykim9050@naver.com>
2024-03-14 14:03:27 +0900

    김경윤 : GameOverUI - 게임오버시 스테이지 정보 랜더링 구현

--- Mario/MarioContents/GameOverUI.cpp
+++ Mario/MarioContents/GameOverUI.cpp
@@ -16,6 +16,15 @@ void AGameOverUI::BeginPlay()
 	UIRenderer->SetImage("GameOver.png");
 	FVector UIScale = UIRenderer->GetImage()->GetScale();
 	UIRenderer->SetTransform({ {UIScale.ihX() * UInGameValue::WindowSizeMulValue, UIScale.ihY() * UInGameValue::WindowSizeMulValue}, {UIScale.iX() * UInGameValue::WindowSizeMulValue, UIScale.iY() * UInGameValue::WindowSizeMulValue} });
+
+	NumberToAnimation(IntroScore, 6, ScoreRenderer);
+	NumberToAnimation(IntroAmountOfCoins, 2, CoinCountRenderer);
+
+	for (int i = 0; i < UInGameValue::StageInfoRendererSize; i++)
+	{
+		StageInfoRenderer[i]->ChangeAnimation(IntroStageInfo[i]);
+		StageInfoRenderer[i]->ChangeAnimation(IntroStageInfo[i]);
+	}
 }
 
 void AGameOverUI::Tick(float _DeltaTime)
