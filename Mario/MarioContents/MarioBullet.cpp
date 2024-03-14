81789e1c64a6622c05e18a358c16effa6c0051bc
kykim9050 <kykim9050@naver.com>
2024-03-14 21:10:32 +0900

    김경윤 : MarioBullet - 애니메이션 랜더링 구현

--- Mario/MarioContents/MarioBullet.cpp
+++ Mario/MarioContents/MarioBullet.cpp
@@ -11,6 +11,25 @@ AMarioBullet::~AMarioBullet()
 void AMarioBullet::BeginPlay()
 {
 	ABulletUnit::BeginPlay();
+
+	Renderer = CreateImageRenderer(static_cast<int>(EStageRenderOrder::Bullet));
+	Renderer->SetImage("FireBall.png");
+	FVector FireBallScale = Renderer->GetImage()->GetScale();
+	Renderer->SetTransform({ {0,0}, {FireBallScale.iX() / UInGameValue::FireBallImageXValue * UInGameValue::WindowSizeMulValue, FireBallScale.iY() / UInGameValue::FireBallImageYValue * UInGameValue::WindowSizeMulValue} });
+	Renderer->CreateAnimation("FiringBullet", "FireBall.png", 0, 3, 0.1f, true);
+	Renderer->CreateAnimation("FireBallDestroy", "FireBall.png", 4, 6, 0.1f, false);
+	Renderer->ChangeAnimation("FiringBullet");
+
+	//Renderer->CreateAnimation("Firing_Right", "KoopaFire_Right.png", 0, 1, 0.1f, true);
+
+	//BodyCollision = CreateCollision(ECollisionOrder::MonsterBullet);
+	//BodyCollision->SetTransform({ { 0,0 }, { UInGameValue::KoopaBulletXColScale, UInGameValue::KoopaBulletYColScale } });
+	//BodyCollision->SetColType(ECollisionType::Rect);
+
+	//Renderer->ActiveOff();
+	//BodyCollision->ActiveOff();
+
+	//SetActorState(EActorState::FirstInit);
 }
 
 void AMarioBullet::Tick(float _DeltaTime)
