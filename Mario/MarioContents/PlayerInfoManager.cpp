617430a2bc0a99ac9e70dfeb1cf81e30f2f6de1b
kykim9050 <kykim9050@naver.com>
2024-03-15 16:29:33 +0900

    김경윤 : PlayerMario - 코드 수정

--- Mario/MarioContents/PlayerInfoManager.cpp
+++ Mario/MarioContents/PlayerInfoManager.cpp
@@ -6,10 +6,8 @@ int UPlayerInfoManager::PlayerLife = 3;
 int UPlayerInfoManager::AmountOfCoins = 0;
 int UPlayerInfoManager::PlayerScore = 0;
 std::vector<std::string> UPlayerInfoManager::StageInfo = std::vector<std::string>({ "1", "1" });
-//EMarioType UPlayerInfoManager::PlayerType = EMarioType::Small;
-EMarioType UPlayerInfoManager::PlayerType = EMarioType::Fire;
-//FVector UPlayerInfoManager::PlayerColSize = FVector{ UInGameValue::PlayerCollisionScaleX, UInGameValue::PlayerCollisionScaleY };
-FVector UPlayerInfoManager::PlayerColSize = FVector{ UInGameValue::BigMarioColScale };
+EMarioType UPlayerInfoManager::PlayerType = EMarioType::Small;
+FVector UPlayerInfoManager::PlayerColSize = FVector{ UInGameValue::PlayerCollisionScaleX, UInGameValue::PlayerCollisionScaleY };
 std::string UPlayerInfoManager::PrevStageLevel = "";
 
 UPlayerInfoManager::UPlayerInfoManager()
